b9ca417a4c47e9b03d11ed4a83bf3bbcc3078e6f
Anders Bohn Jespersen <anders.jespersen@teralytics.ch>
2018-11-02 17:03:32 +0100

    .

--- vars/stdSbt.groovy
+++ vars/stdSbt.groovy
@@ -1,100 +1,103 @@
 def call(configOverrides) {
 
-  def config = [
-     releaseBranch: ['*/master','master'],
-     autoVersionRelease: false 
-  ]
+    def config = [
+            releaseBranch     : ['*/master', 'master'],
+            autoVersionRelease: false
+    ]
 
-  configOverrides.resolveStrategy = Closure.DELEGATE_FIRST
-  configOverrides.delegate = config
-  
-  configOverrides()
+    configOverrides.resolveStrategy = Closure.DELEGATE_FIRST
+    configOverrides.delegate = config
 
-  def branchName = scm.branches[0].name //env.BRANCH_NAME
+    configOverrides()
 
-  def isReleaseBranch = config.releaseBranch.contains(branchName)
+    def branchName = scm.branches[0].name //env.BRANCH_NAME
 
-  echo "std sbt called with config ${config} - ${isReleaseBranch} - ${branchName}"  
+    def isReleaseBranch = config.releaseBranch.contains(branchName)
 
-  try {
-    node('master') {  
-    	stage('checkout') {
-    		scmResult = checkout scm
-        echo "scm " + scmResult
-    	}
-      stage('Build') {
-	      echo "Std Build"
+    echo "std sbt called with config ${config} - ${isReleaseBranch} - ${branchName}"
 
-      }
-      stage('Test') {
-        echo "Std Test" 	
-      }
-      
-
-      if (isReleaseBranch) {
-      	if (!config.autoVersionRelease) {
-      		stage("Confirm Release") {
-              echo "mkdir"
-              
-              
-              echo "write file - " + scmResult.GIT_URL
-
-              def currentVersion = 'v1.0.0'
-
-              echo "mkdir"
-              sh 'mkdir -p target/buildsummary'
-              echo "write buildsummary"
-              
-              def htmlString = '<html><body>Create release for ' + env.BRANCH_NAME + ' -> <a href="https://github.com/andersbohn/jdemoprj/releases/new?tag='+currentVersion+'">on github release page</a></body></html>'
-              sh 'echo "'+htmlString+'" > target/buildsummary/index.html'
-              echo "publish Html "
-              publishHTML target: [
-                          allowMissing:true,
-                          alwaysLinkToLastBuild: false,
-                          keepAll:true,
-                          reportDir: 'target/buildsummary',
-                          reportFiles: 'index.html',
-                          reportName: 'BuildSummary'
-                      ]
-
-              echo "version link - " + currentVersion 
-              def versionHtml = '<a href="'+ env.BUILD_NUMBER + '/BuildSummary">' + currentVersion + '</p>'
-              addHtmlBadge html: versionHtml, id: 'versionlink'
-
-            /*def proceedWithRelease = true
-            try { 
-      			  milestone()
-              link()
-      			  //input(id: "Confirmation", message: "Confirm Release")
-      			  milestone()
-            } catch(e) {
-              proceedWithRelease = false
+    try {
+        node('master') {
+            stage('checkout') {
+                scmResult = checkout scm
+                echo "scm " + scmResult
             }
-            if (proceedWithRelease) {
-              stage("Release") {
-                echo "Released by confirmation"
-              }
-            }*/
-      		}
-      	} else {
-      	  stage("Release") {
-            echo "Auto Released"
-          }
-        }
+            stage('Build') {
+                echo "Std Build"
 
-      } else {
-      	stage("Publish") {
-		      echo "Std Publish"	
-      	}
-      }
-      stage("Clean") {
-	     	echo "Std Clean"	
-      }
+            }
+            stage('Test') {
+                echo "Std Test"
+            }
+
+
+            if (isReleaseBranch) {
+                if (!config.autoVersionRelease) {
+                    stage("Confirm Release") {
+                        echo "mkdir"
+
+
+                        echo "write file - " + scmResult.GIT_URL
+
+                        def currentVersion = 'v1.0.0'
+
+                        echo "mkdir"
+                        sh 'mkdir -p target/buildsummary'
+                        echo "write buildsummary"
+
+                        def htmlString = '<html><body>Create release for ' + env.BRANCH_NAME + ' -> <a href="https://github.com/andersbohn/jdemoprj/releases/new?tag=' + currentVersion + '">on github release page</a></body></html>'
+                        sh 'echo "' + htmlString + '" > target/buildsummary/index.html'
+                        echo "publish Html "
+                        publishHTML target: [
+                                allowMissing         : true,
+                                alwaysLinkToLastBuild: false,
+                                keepAll              : true,
+                                reportDir            : 'target/buildsummary',
+                                reportFiles          : 'index.html',
+                                reportName           : 'BuildSummary'
+                        ]
+
+                        echo "version link - " + currentVersion
+                        def versionHtml = '<a href="' + env.BUILD_NUMBER + '/BuildSummary">' + currentVersion + '</p>' +
+                                '<script>' +
+                                'console.log(\'test\');' +
+                                '</script>'
+                        addHtmlBadge html: versionHtml, id: 'versionlink'
+
+                        /*def proceedWithRelease = true
+                        try {
+                                milestone()
+                          link()
+                                //input(id: "Confirmation", message: "Confirm Release")
+                                milestone()
+                        } catch(e) {
+                          proceedWithRelease = false
+                        }
+                        if (proceedWithRelease) {
+                          stage("Release") {
+                            echo "Released by confirmation"
+                          }
+                        }*/
+                    }
+                } else {
+                    stage("Release") {
+                        echo "Auto Released"
+                    }
+                }
+
+            } else {
+                stage("Publish") {
+                    echo "Std Publish"
+                }
+            }
+            stage("Clean") {
+                echo "Std Clean"
+            }
+        }
+    } catch (err) {
+        echo "error - cleaning up ${err}"
+        stage("Clean") {
+            echo "Std Clean After error"
+        }
     }
-  } catch(err) {
-  	echo "error - cleaning up ${err}"
-  	stage("Clean") {
-	  echo "Std Clean After error"	
-    }
-  } 
 }
